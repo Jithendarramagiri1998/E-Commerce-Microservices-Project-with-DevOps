743a19c0b012ed4c1a848ca0466cb97beb2c7e4c
Jithendarramagiri <jithendarramagiri1998@gmail.com>
2025-02-08 19:29:09 +0000

    Initial setup

---
+++ ecommerce-microservices/user-service/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("User Service Running"));
+app.listen(5001, () => console.log("User Service running on port 5001"));
+
